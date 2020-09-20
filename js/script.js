aebce002d1659ec55ab76df6d8eea1dd18cc0742
Victor <kozykin_victor_95@mail.ru>
2020-09-20 16:22:35 +0700

    5hw

--- js/script.js
+++ js/script.js
@@ -306,7 +306,7 @@ const calc = (a, b) => { return a + b }; //  если код в 1 строчку
 */
 
 /////методы и свойства строк и чисел  ////////////////////////////////////////////////////////////////////////////
-
+/*
 const str = 'test';
 
 console.log(str.length);
@@ -335,3 +335,254 @@ console.log(Math.round(num));
 const test = '12.2px';
 console.log(parseInt(test)); // метод преодразует в числовой вид данных (целое)
 console.log(parseFloat(test)); // метод преодразует в числовой вид данных (дробное)
+
+*/
+
+/////Callback функции  ////////////////////////////////////////////////////////////////////////////
+/*
+function first() {
+    // do smth
+    setTimeout(function()  {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+// callback -функция, которая должна быть выполнена после того, как другая функция завершила свое выполнение
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок')
+}
+
+learnJS ('JS', done); //круглые скобки не ставим, т.к. мы не вызываем функцию, а передаем ее
+*/
+
+/////Объекты , деструктуризация объектов ////////////////////////////////////////////////////////////////////////////
+
+/*
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {   //метод, встроенный в объект
+        console.log('TEst');
+    }
+};
+
+console.log(options.name);
+
+//delete options.name;  // удаление
+
+console.log(options);
+
+let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {console.log(`Свойство ${key} имеет значение ${options[key]}`);
+            counter++;
+        }
+}
+
+console.log(counter);
+
+console.log(Object.keys(options).length);  // создает массив из ключей
+
+options.makeTest();
+
+
+//деструктуризация
+
+const {border, bg} = options.colors; // вытаскиваем свойства из ключа из ключа colors в качестве отдельных переменных
+console.log(border, bg); //печатает black, red
+*/
+
+/////Массивы и псевдомассивы////////////////////////////////////////////////////////////////////////////
+/*
+const arr = [1, 22, 3, 4, 8];
+// arr[99] = 0;
+// console.log(arr.length);
+
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`)
+});
+
+
+arr.pop();  //удаляет последний элемент массива
+console.log(arr);
+arr.push(10); // добавляет в конец 
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {  // of работает с массивами,map,set
+    console.log(value);     //можно использовать break, continue
+}
+
+const str = prompt('', '');
+const products = str.split(', '); // создает массив из строки с указанием разделителя
+products.sort(); // сортирует как строки по алфавиту
+console.log(products.join('; ')); // создает строку из массива с разделителем
+*/
+
+
+/////Передача по ссылке или по значению, Spread оператор//////////////////////////////////////////////////
+/*
+let a = 5,
+    b = a;
+
+b = b+ 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; // ссылка на объект // передача по ссылке
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// // копия объекта
+
+function copy(mainObj) { // поверхностная копия
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+
+console.log(numbers);
+console.log(newNumbers);
+
+const add = {
+    d:17,
+    e:20
+};
+
+console.log(Object.assign(numbers, add)); // слияние 2 массивов
+
+const clon = Object.assign({}, add);
+
+clon.d = 20;
+
+console.log(clon); 
+
+
+// копия массива
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'sddfswe'
+console.log(newArray, oldArray);
+
+//оператор для разворота 
+const video = ['youtube', 'video', 'rutubr'],
+      blogs = ['dfdf', 'dfdf', 'sdsdwe'],
+      inernet = [...video, ...blogs, 'dfd', 'er3d'];
+console.log(inernet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 6];
+
+log(...num);
+
+// 4 способ поверхностного клон объ
+
+const array = ['a', 'b'];
+const q = [...array];
+
+const qw = {
+    one: 1,
+    two: 2
+};
+
+const newOb = {...qw};
+console.log(newOb);
+*/
+/////Основы ООП (прототип-ориент-наследование//////////////////////////////////////////////////////////////////
+
+
+// let str = 'sone';
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+/*
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+const john1 = Object.create(soldier); // новый метод
+
+const john = {
+    health: 100
+};
+
+// john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+
+console.log(john.armor);
+john.sayHello();
+
+*/
+/////Ошибки в коде в консоли разработчика////
